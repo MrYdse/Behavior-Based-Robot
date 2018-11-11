c280b3a2206f7cca90e1786ce7717e0cb16186d5
MrYdse <er.yd@outlook.com>
2018-11-11 16:44:12 +0100

    Added wander to initialization

--- DuckHater.py
+++ DuckHater.py
@@ -8,15 +8,22 @@ if __name__ == '__main__':
     button = ZumoButton()
     button.wait_for_press()
     BBCON = BBCON()
-    behaviors = [Stop(BBCON, 1.0), DontFall(BBCON, 1.0),
-                 ViolateDuck(BBCON, 0.8), FindDuck(BBCON, 0.8)]
-    BBCON.active_behaviors = BBCON.behaviors()
-    sensobs = {"fallingout": FallingOut(), "duckfinder": DuckFinder(),
-               "buttonfeeler": ButtonFeeler(), "peeper": Peeper()}
-    BBCON.add_motob(Motob())
-    for behavior in behaviors:
-        BBCON.add_behavior(behavior)
+    fall = FallingOut()
+    time.sleep(1)
+    duck = DuckFinder()
+    time.sleep(1)
+    butt = ButtonFeeler()
+    peep = Peeper()
+    time.sleep(1)
+    sensobs = {"fallingout": fall, "duckfinder": duck,
+               "buttonfeeler": butt, "peeper": peep}
     for sensob in sensobs:
         BBCON.add_sensob(sensob, sensobs[sensob])
+    behaviors = [Stop(BBCON, 1.0), DontFall(BBCON, 1.0),
+                 ViolateDuck(BBCON, 0.8), FindDuck(BBCON, 0.8), Wander(BBCON, 0.1)]
+    for behavior in behaviors:
+        BBCON.add_behavior(behavior)
+    BBCON.active_behaviors = BBCON.behaviors
+    BBCON.add_motob(Motob())
     while not BBCON.halt:
         BBCON.run_one_timestep()
