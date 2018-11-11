ea7a5efcd22ad560117793259654e864d2b53404
MrYdse <er.yd@outlook.com>
2018-11-11 15:12:54 +0100

    Added new sensobs and behaviors

--- DuckHater.py
+++ DuckHater.py
@@ -3,12 +3,15 @@ from Motob import *
 from FindAndFall import *
 from Stop import Stop
 from DontFall import DontFall
+from ViolateDuck import ViolateDuck
+from FindDuck import FindDuck
 
 if __name__ == '__main__':
     BBCON = BBCON()
-    behaviors = [Stop(BBCON, 1.0), DontFall(BBCON, 1.0)]
+    behaviors = [Stop(BBCON, 1.0), DontFall(BBCON, 1.0),
+                 ViolateDuck(BBCON, 0.8), FindDuck(BBCON, 0.8)]
     sensobs = {"fallingout": FallingOut(), "duckfinder": DuckFinder(),
-               "buttonfeeler": ButtonFeeler()}
+               "buttonfeeler": ButtonFeeler(), "peeper": Peeper()}
     BBCON.add_motob(Motob())
     for behavior in behaviors:
         BBCON.add_behavior(behavior)
